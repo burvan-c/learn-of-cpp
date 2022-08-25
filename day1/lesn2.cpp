99f98ad028a9da510e6d4a3cead6410b85aa3f9f
2447146432 <2447146432@qq.com>
2022-08-25 21:57:47 +0800

    learning

--- day1/lesn2.cpp
+++ day1/lesn2.cpp
@@ -6,22 +6,66 @@ using namespace std;
 #include<stdio.h>
 
 
-struct stu
-{
-	string name;
-	int age;
-	int score;
-};
-int main()
-{
-	struct stu s1;
-	//struct stu s1={"张三",18,100};
-	s1.name = "张三";
-	s1.age = 18;
-	s1.score = 100;
-	cout << "姓名：" << s1.name << "年纪：" << s1.age << "分数：" << s1.score;
+//struct stu
+//{
+//	string name;
+//	int age;
+//	int score;
+//};
+//int main()
+//{
+//	struct stu s1;
+//	//struct stu s1={"张三",18,100};
+//	//定义结构体时直接初始化
+//	s1.name = "张三";
+//	s1.age = 18;
+//	s1.score = 100;
+//	cout << "姓名：" << s1.name << "年纪：" << s1.age << "分数：" << s1.score;
+//
+//
+//	system("pause");
+//	return 0;
+//}
 
 
-	system("pause");
-	return 0;
-}
+
+//结构体数组
+//struct  结构体名   数组名【元素个数】={{}，{}，。。。}
+// 
+//struct stu
+//{
+//	string name;
+//	int age;
+//	int score;
+//};
+//int main()
+//{
+//	//结构体数组初始化
+//	struct stu stary[3] =
+//	{
+//		{"张三",10,100},
+//		{"李四",14,95},
+//		{"王五",20,30},
+//	};
+//
+//	//结构体数组元素重新赋值
+//	stary[2].name = "赵四";
+//	stary[2].age = 60;
+//	stary[2].score = 66;
+//
+//	for (int i = 0; i < 3; i++)
+//	{
+//		cout << stary[i].name <<"\t"<< stary[i].age << "\t" << stary[i].score;
+//		cout << endl;
+//	}
+//
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//结构体指针
