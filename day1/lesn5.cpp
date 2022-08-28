ccbbec3420c39a4f331db436d522fdddeb476c7c
2447146432 <2447146432@qq.com>
2022-08-28 21:41:15 +0800

    learn of cpp

--- day1/lesn5.cpp
+++ day1/lesn5.cpp
@@ -2,12 +2,34 @@
 #pragma once
 #include<iostream>
 using namespace std;
-#include<stdio.h>
 #include<string>
+#define MAX 1000
 
 
 //通讯录管理系统
 
+
+//联系人结构体
+struct Person
+{
+	string name;
+	int m_sex;//1男2女
+	int m_age;
+	string m_phine;
+	string m_addr;
+};
+//通讯录结构体
+struct Addressbooks
+{
+	//保存联系人数组
+	struct Person pArray[MAX];
+	//通讯录当前记录人数个数
+	int m_size;
+};
+void addPerson(Addressbooks* abs)
+{
+
+}
 //菜单
 void showMenu()
 {
@@ -19,11 +41,16 @@ void showMenu()
 	cout << "***** 6.清空联系人 *****" << endl;
 	cout << "***** 0.退出通讯录 *****" << endl;
 }
+
 int main()
 {
+	//创建通讯录结构体变量
+	Addressbooks abs;
+	//初始化通讯录
+	abs.m_size = 0;
+
 	int select = 0;//创建用户输入变量
 
-	
 	while (true)
 	{
 		showMenu();
@@ -31,6 +58,7 @@ int main()
 		switch (select)
 		{
 		case 1://1.添加联系人
+			addPerson(&abs);//地址传递才可以改变实参
 			break;
 		case 2://2.显示联系人
 			break;
