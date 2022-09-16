174b247b51e8d1e3a158a38e728684872669e009
2447146432 <2447146432@qq.com>
2022-09-16 22:18:23 +0800

    learn of cpp

--- day2/lesn5.cpp
+++ day2/lesn5.cpp
@@ -2,9 +2,9 @@
 //#pragma once
 
 
-#include<iostream>
-using namespace std;
-#include<string>
+//#include<iostream>
+//using namespace std;
+//#include<string>
 
 
 //对象模型、this指针
@@ -422,6 +422,211 @@ using namespace std;
 //int main()
 //{
 //	test01();
+//	system("pause");
+//	return 0;
+//}
+
+
+//递增运算符重载--实现自己的整形数据
+
+//自定义整形
+//class myinteger
+//{
+//	friend ostream& operator<<(ostream& cout, myinteger myint);
+//public:
+//	myinteger()
+//	{
+//		m_num = 0;
+//	}
+//
+//	//重载前置++运算符
+//	myinteger& operator++()//返回引用是为了一直对一个数据进行递增操作
+//	{
+//		m_num++;
+//		return *this;
+//	}
+//
+//	//重载前置++运算符
+//	//void operator++(int)  int 代表占位参数，用于区分前置和后置递增
+//	myinteger operator++(int)
+//	{
+//		myinteger tmp = *this;
+//		m_num++;
+//		return tmp;
+//	}
+//
+//private:
+//	int m_num;
+//};
+//
+//重载左移运算符
+//ostream& operator<<(ostream& cout, myinteger myint)
+//{
+//	cout << myint.m_num;
+//	return cout;
+//}
+//void test01()
+//{
+//	myinteger myint;
+//	cout << ++myint << endl;
+//}
+//void test02()
+//{
+//	myinteger myint;
+//	cout << myint++ << endl;
+//	cout << myint << endl;
+//}
+//int main()
+//{
+//	//test01();
+//	test02();
+//	system("pause");
+//	return 0;
+//}
+
+
+//赋值运算符重载
+
+//c++编译器至少给一个类添加4个函数
+//1.默认构造函数 2.默认析构函数 3.默认拷贝构造函数 4.赋值运算符operator=对属性进行值拷贝
+
+//class Person
+//{
+//public:
+//	Person(int age)
+//	{
+//		m_age = new int(age);
+//	}
+//
+//	~Person()
+//	{
+//		if (NULL != m_age)
+//		{
+//			delete m_age;
+//			m_age = NULL;
+//		}
+//	}
+//
+//	//重载 赋值运算符
+//	Person& operator=(Person&p)
+//	{
+//		//先判断是否有属性，若有先释放，再深拷贝
+//		if(m_age!=NULL)
+//		{
+//			delete m_age;
+//			m_age = NULL;
+//		}
+//		m_age = new int(*p.m_age);//深拷贝
+//		return *this;
+//	}
+//
+//	int* m_age; 
+//};
+//
+//void test01()
+//{
+//	Person p1(18);
+//	Person p2(20);
+//	Person p3(30);
+//
+//	p3 = p2 = p1;//这里会浅拷贝，把值拷贝过去，堆区内容重复释放，程序崩溃！
+//
+//	cout << "p1年龄：" << *p1.m_age << endl;
+//	cout << "p2年龄：" << *p2.m_age << endl;
+//	cout << "p3年龄：" << *p3.m_age << endl;
+//}
+//
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//}
+
+
+//关系运算符重载
+
+//class Person
+//{
+//public:
+//	Person(string name, int age)
+//	{
+//		m_name = name;
+//		m_age = age;
+//	}
+//
+//	//重载==号
+//	bool operator==(Person& p)
+//	{
+//		if (this->m_name == p.m_name && this->m_age == p.m_age)
+//		{
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//	string m_name;
+//	int m_age;
+//};
+//void test01()
+//{
+//	Person p1("tom", 18);
+//	Person p2("tomy", 18);
+//
+//	if (p1 == p2)
+//	{
+//		cout << "p1与p2相等" << endl;
+//	}
+//	else
+//	{
+//		cout << "p1与p2不相等" << endl;
+//	}
+//}
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//}
+
+
+//函数调用运算（）符重载，也称为仿函数 
+
+//class myprint
+//{
+//public:
+//	//重载函数调用运算符
+//	void operator()(string test)
+//	{
+//		cout << test << endl;
+//	}
+//};
+//class myadd
+//{
+//public:
+//	int operator()(int num1, int num2)
+//	{
+//		return num1 + num2;
+//	}
+//};
+//void test01()
+//{
+//	myprint myprint;//使用时很类似函数调用，又称为仿函数
+//	myprint("hello world");
+//}
+//void test02()
+//{
+//	myadd myadd1;
+//	int ret=myadd1(100, 100);
+//	cout << ret << endl;
+//
+//	//匿名函数对象--当前执行完立即被释放
+//	cout << myadd()(100, 100) << endl;
+//}
+//int main()
+//{
+//	//test01();
+//	test02();
 //	system("pause");
 //	return 0;
 //}
