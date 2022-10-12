ae67fa9db8be8a30e09591f64ae849caa2814c51
2447146432 <2447146432@qq.com>
2022-10-12 21:51:05 +0800

    learn of cpp

---
+++ day2/case3.h
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+#pragma once//防止头文件重复包含
+#include<iostream>
+using namespace std;
+
+class workermanager
+{
+public:
+	workermanager();
+
+	void show_menu();
+
+	void exitsystem();
+
+	~workermanager();
+};
